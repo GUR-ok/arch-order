77cfbf47cee06c6a1f9eaaf68fcc990f6e302096
Yuriy Gorelov <gur-ok@yandex.ru>
2022-11-20 21:43:10 +0400

    fix groupid kafka, fix handlers

--- src/main/java/ru/gur/archorder/service/kafka/eventhandler/OrderPaidHandler.java
+++ src/main/java/ru/gur/archorder/service/kafka/eventhandler/OrderPaidHandler.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import ru.gur.archorder.entity.State;
 import ru.gur.archorder.service.kafka.Event;
 import ru.gur.archorder.service.kafka.EventSource;
 import ru.gur.archorder.service.kafka.OrderPaidEventData;
+import ru.gur.archorder.service.order.OrderService;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OrderPaidHandler implements EventHandler<OrderPaidEventData> {
+
+    private final OrderService orderService;
 
     @Override
     public boolean canHandle(final EventSource eventSource) {
@@ -24,7 +28,7 @@ public class OrderPaidHandler implements EventHandler<OrderPaidEventData> {
     public String handleEvent(final OrderPaidEventData eventSource) {
         Assert.notNull(eventSource, "EventSource must not be null");
 
-//        service.call
+        orderService.updateStatus(eventSource.getOrderId(), State.APPROVED);
         System.out.printf("!!! ORDER %s PAID SUCCESSFULLY\n", eventSource.getOrderId());
 
         log.info("Event handled: {}", eventSource);
--- src/main/java/ru/gur/archorder/service/kafka/eventhandler/PaymentFailHandler.java
+++ src/main/java/ru/gur/archorder/service/kafka/eventhandler/PaymentFailHandler.java
@@ -4,14 +4,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
+import ru.gur.archorder.entity.State;
 import ru.gur.archorder.service.kafka.Event;
 import ru.gur.archorder.service.kafka.EventSource;
 import ru.gur.archorder.service.kafka.PaymentFailEventData;
+import ru.gur.archorder.service.order.OrderService;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class PaymentFailHandler implements EventHandler<PaymentFailEventData> {
+
+    private final OrderService orderService;
 
     @Override
     public boolean canHandle(final EventSource eventSource) {
@@ -24,7 +28,7 @@ public class PaymentFailHandler implements EventHandler<PaymentFailEventData> {
     public String handleEvent(final PaymentFailEventData eventSource) {
         Assert.notNull(eventSource, "EventSource must not be null");
 
-//        service.call
+        orderService.updateStatus(eventSource.getOrderId(), State.CANCELED);
         System.out.printf("!!! NOT ENOUGH MONEY on account %s for order %s\n",
                 eventSource.getAccountId(), eventSource.getOrderId());
 
